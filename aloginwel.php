4d4256257f75e32b3afc8e8e69d47500e4d8259e
Sachintha Gunaratne <sachinthasl99@gmail.com>
2023-10-03 23:26:09 +0530

    admin welcome query fixed

--- aloginwel.php
+++ aloginwel.php
@@ -1,6 +1,6 @@
 <?php 
 require_once ('process/dbh.php');
-$sql = "SELECT id, firstName, lastName,  points FROM employee, rank WHERE rank.eid = employee.id order by rank.points desc";
+$sql = "SELECT id, firstName, lastName, points FROM employee, `rank` WHERE rank.eid = employee.id order by rank.points desc";
 $result = mysqli_query($conn, $sql);
 ?>
 
